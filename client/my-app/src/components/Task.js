3a7193eeeb9eb149b6d180621a39104518dbb239
TairFarangi <59148532+TairFarangi@users.noreply.github.com>
2022-11-30 22:55:10 +0200

    change

--- client/my-app/src/components/Task.js
+++ client/my-app/src/components/Task.js
@@ -3,7 +3,7 @@ function Task(props) {
     return <div>
         <p>
             <label> {props.data.taskText}</label>
-            &emsp;&emsp;&emsp;&emsp;
+            &emsp;&emsp;&emsp;&emsp;&emsp;
             <label> {props.data.dueDate}</label>
         </p>
     </div >
